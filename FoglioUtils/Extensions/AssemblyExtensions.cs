eacf7b63808bda85416b558a85b4c285c868b0f2
Foglio1024 <foglio1024@gmail.com>
2019-06-28 11:18:38 +0200

    add assembly extension methods

---
+++ FoglioUtils/Extensions/AssemblyExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoglioUtils.Extensions
+{
+    public static class AssemblyExtensions
+    {
+        public static Stream GetResourceStream(this Assembly assembly, string name)
+        {
+            return assembly.GetManifestResourceStream(assembly.GetManifestResourceNames().Single(x => x.EndsWith(name)));
+        }
+    }
+}
