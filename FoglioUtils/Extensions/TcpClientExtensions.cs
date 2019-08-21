c3fcf91b372619907d48a09b68317c7bd5bdd465
Foglio1024 <foglio1024@gmail.com>
2019-08-21 18:46:34 +0200

    add tcpclient ext method

---
+++ FoglioUtils/Extensions/TcpClientExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Sockets;
+
+namespace FoglioUtils.Extensions
+{
+    public static class TcpClientExtensions
+    {
+        public static bool IsConnected(this TcpClient client)
+        {
+            if (!client.Client.Poll(0, SelectMode.SelectRead)) return false;
+            return client.Client.Receive(new byte[1], SocketFlags.Peek) != 0;
+        }
+    }
+}
