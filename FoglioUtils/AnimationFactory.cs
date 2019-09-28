50248aac6e31151f62135e5e188ded8cfdb757f3
Foglio1024 <foglio1024@gmail.com>
2019-09-28 21:44:16 +0200

    added AnimationFactory class

---
+++ FoglioUtils/AnimationFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Animation;
+
+namespace FoglioUtils
+{
+    public static class AnimationFactory
+    {
+        public static DoubleAnimation CreateDoubleAnimation(int ms, double to, double from = double.NaN, bool easing = false, EventHandler completed = null)
+        {
+            var ret = new DoubleAnimation
+            {
+                Duration = TimeSpan.FromMilliseconds(ms),
+                To = to
+            };
+            if (!double.IsNaN(from)) ret.From = from;
+            if (easing) ret.EasingFunction = new QuadraticEase();
+            if (completed != null) ret.Completed += completed;
+            return ret;
+        }
+    }
+}
