80adba646bb69980811aa61e8d9baf32f426ce51
Foglio1024 <foglio1024@gmail.com>
2020-01-27 17:25:43 +0100

    add checkbox attached prop

--- Nostrum/Extensions/ComboBoxExtensions.cs
+++ Nostrum/Extensions/ComboBoxExtensions.cs
@@ -12,4 +12,17 @@ namespace Nostrum.Extensions
         public static Style GetDropDownBorderStyle(DependencyObject obj) => (Style)obj.GetValue(DropDownBorderStyleProperty);
         public static void SetDropDownBorderStyle(DependencyObject obj, Style value) => obj.SetValue(DropDownBorderStyleProperty, value);
     }
+
+    public class CheckBoxExtensions
+    {
+
+        public static readonly DependencyProperty SizeProperty = DependencyProperty.RegisterAttached("Size", 
+                                                                                                     typeof(double), 
+                                                                                                     typeof(CheckBoxExtensions), 
+                                                                                                     new PropertyMetadata(18D));
+        public static double GetSize(DependencyObject obj) => (double)obj.GetValue(SizeProperty);
+        public static void SetSize(DependencyObject obj, double value) => obj.SetValue(SizeProperty, value);
+
+
+    }
 }
