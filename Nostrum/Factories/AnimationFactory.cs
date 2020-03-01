b77c60a1dedd8078018b9290a03b0fe9416424bb
Foglio1024 <foglio1024@gmail.com>
2020-03-01 20:54:39 +0100

    add delay to AnimationFactory methods

--- Nostrum/Factories/AnimationFactory.cs
+++ Nostrum/Factories/AnimationFactory.cs
@@ -7,12 +7,13 @@ namespace Nostrum.Factories
 {
     public static class AnimationFactory
     {
-        public static DoubleAnimation CreateDoubleAnimation(int ms, double to, double from = double.NaN, bool easing = false, EventHandler completed = null, int framerate = 60)
+        public static DoubleAnimation CreateDoubleAnimation(int ms, double to, double from = double.NaN, bool easing = false, EventHandler completed = null, int framerate = 60, int delay = 0)
         {
             var ret = new DoubleAnimation
             {
                 Duration = TimeSpan.FromMilliseconds(ms),
-                To = to
+                To = to,
+                BeginTime = TimeSpan.FromMilliseconds(delay)
             };
             if (!double.IsNaN(from)) ret.From = from;
             if (easing) ret.EasingFunction = new QuadraticEase();
@@ -21,21 +22,22 @@ namespace Nostrum.Factories
             return ret;
         }
         
-        public static ThicknessAnimation CreateThicknessAnimation(int ms, Thickness to, bool easing = false, EventHandler completed = null, int framerate = 60)
+        public static ThicknessAnimation CreateThicknessAnimation(int ms, Thickness to, bool easing = false, EventHandler completed = null, int framerate = 60, int delay = 0)
         {
             var ret = new ThicknessAnimation
             {
                 Duration = TimeSpan.FromMilliseconds(ms),
-                To = to
+                To = to,
+                BeginTime = TimeSpan.FromMilliseconds(delay)
             };
             if (easing) ret.EasingFunction = new QuadraticEase();
             if (completed != null) ret.Completed += completed;
             Timeline.SetDesiredFrameRate(ret, framerate);
             return ret;
         }
-        public static ThicknessAnimation CreateThicknessAnimation(int ms, Thickness to, Thickness from, bool easing = false, EventHandler completed = null, int framerate = 60)
+        public static ThicknessAnimation CreateThicknessAnimation(int ms, Thickness to, Thickness from, bool easing = false, EventHandler completed = null, int framerate = 60, int delay = 0)
         {
-            var ret = CreateThicknessAnimation(ms, to, easing: easing, completed, framerate);
+            var ret = CreateThicknessAnimation(ms, to, easing: easing, completed, framerate, delay);
             ret.From = from;
             return ret;
         }
