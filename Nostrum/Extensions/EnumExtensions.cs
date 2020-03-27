84f7e0b16ff856a4a8f9dbecb6b64c8d8f9bcf0c
Foglio1024 <foglio1024@gmail.com>
2020-03-27 17:54:27 +0100

    add GetDescription() enum extension method

---
+++ Nostrum/Extensions/EnumExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Nostrum.Extensions
+{
+    public static class EnumExtensions
+    {
+        /// <summary>
+        /// Returns an enumerator description attribute if present, else the enum as string.
+        /// </summary>
+        /// <param name="en"></param>
+        /// <returns></returns>
+        public static string GetDescription(this Enum en)
+        {
+            var fi = en.GetType().GetField(en.ToString());
+            var attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return ((attributes.Length > 0)
+                    && (!string.IsNullOrEmpty(attributes[0].Description)))
+                ? attributes[0].Description
+                : en.ToString();
+        }
+    }
+}
