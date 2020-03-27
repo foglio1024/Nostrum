ce9ed2ff4bca473de086ed88bba5ddcced61ce8a
Foglio1024 <foglio1024@gmail.com>
2020-03-27 17:54:53 +0100

    add GetDataContext<T> FrameworkElement extension method

---
+++ Nostrum/Extensions/FrameworkElementExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace Nostrum.Extensions
+{
+    public static class FrameworkElementExtensions
+    {
+        /// <summary>
+        /// Returns the DataContext of the <see cref="FrameworkElement"/>, casting it to T.
+        /// Returns null if the cast fails.
+        /// </summary>
+        /// <typeparam name="T">return type</typeparam>
+        /// <returns>the DataContext casted to T</returns>
+        public static T GetDataContext<T>(this FrameworkElement fe) where T : class
+        {
+            if (!(fe.DataContext is T t)) return null;
+            return t;
+        }
+    }
+}
