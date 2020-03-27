350fc95f3c88c2c127b1d447ffbf807f3bd24164
Foglio1024 <foglio1024@gmail.com>
2020-03-27 17:52:48 +0100

    add kernel32 wrappers

---
+++ Nostrum/WinAPI/Kernel32.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Nostrum.WinAPI
+{
+    public static class Kernel32
+    {
+        [DllImport("kernel32.dll")]
+        public static extern int GetCurrentThreadId();
+
+        [DllImport("kernel32.dll")]
+        public static extern bool AllocConsole();
+
+        [DllImport("kernel32.dll")]
+        public static extern void FreeConsole();
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr GetConsoleWindow();
+    }
+}
