574765b9cae989b947a23280fe102a3911032120
Foglio1024 <foglio1024@gmail.com>
2020-10-01 15:25:45 +0200

    add autoscroll behavior to TextBoxExtensions

--- Nostrum/Extensions/TextBoxExtensions.cs
+++ Nostrum/Extensions/TextBoxExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
 
 namespace Nostrum.Extensions
@@ -18,6 +19,43 @@ namespace Nostrum.Extensions
         public static void SetFocusBorderBrush(DependencyObject obj, Brush value)
         {
             obj.SetValue(FocusBorderBrushProperty, value);
+        }
+
+        public static readonly DependencyProperty AutoScrollEnabledProperty = DependencyProperty.RegisterAttached(
+            "AutoScrollEnabled",
+            typeof(bool),
+            typeof(TextBoxExtensions),
+            new UIPropertyMetadata(false, OnAutoScrollEnabledChanged));
+
+        private static void OnAutoScrollEnabledChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (!(d is TextBox tb)) return;
+            var enabled = e.NewValue is bool b && b;
+
+            if (enabled)
+            {
+                tb.TextChanged += OnTextChanged;
+            }
+            else
+            {
+                tb.TextChanged -= OnTextChanged;
+            }
+        }
+
+        private static void OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!(sender is TextBox tb)) return;
+            tb.ScrollToEnd();
+        }
+
+        public static bool GetAutoScrollEnabled(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(AutoScrollEnabledProperty);
+        }
+
+        public static void SetAutoScrollEnabled(DependencyObject obj, bool value)
+        {
+            obj.SetValue(AutoScrollEnabledProperty, value);
         }
     }
 }
