d2342da89235ec7db47a7cf136431134fee1da48
Foglio1024 <foglio1024@gmail.com>
2020-10-01 15:51:13 +0200

    refactor generic relaycommand

--- Nostrum/RelayCommand.cs
+++ Nostrum/RelayCommand.cs
@@ -8,12 +8,12 @@ namespace Nostrum
         private readonly Action<object> _execute;
         private readonly Func<object, bool> _canExecute;
 
-        public bool CanExecute(object parameter)
+        public virtual bool CanExecute(object parameter)
         {
             return _canExecute == null || _canExecute(parameter);
         }
 
-        public void Execute(object parameter)
+        public virtual void Execute(object parameter)
         {
             _execute(parameter);
         }
@@ -30,7 +30,7 @@ namespace Nostrum
             _canExecute = canExecute;
         }
 
-        public RelayCommand(Action execute, Func<object, bool> canExecute = null)
+        public RelayCommand(Action execute, Func<object, bool> canExecute = null) 
         {
             _execute = _ => execute();
             _canExecute = canExecute;
@@ -39,46 +39,15 @@ namespace Nostrum
     /// <summary>
     /// A RelayCommand which allows you to specify command parameter type.
     /// </summary>
-    public class RelayCommand<ParameterType> : ICommand
+    public class RelayCommand<ParameterType> : RelayCommand 
     {
-        private readonly Action<ParameterType> _execute;
-        private readonly Func<object, bool> _canExecute;
-        private readonly bool _checkParamType;
-
-        public bool CanExecute(object parameter)
-        {
-            return _canExecute == null || _canExecute(parameter);
-        }
-
-        public void Execute(object parameter)
-        {
-            if (_checkParamType)
+        public RelayCommand(Action<ParameterType> execute, Func<object, bool> canExecute = null) 
+            : base(o =>
             {
-                if (!(parameter is ParameterType p)) return;
-                _execute(p);
-            }
-            else
-            {
-                _execute((ParameterType) parameter);
-            }
-        }
-
-        public event EventHandler CanExecuteChanged
-        {
-            add => CommandManager.RequerySuggested += value;
-            remove => CommandManager.RequerySuggested -= value;
-        }
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="execute">the Action te executed</param>
-        /// <param name="canExecute">the Func to be executed to check if the command can be executed</param>
-        /// <param name="checkParamType">if true, avoids executing the command if the parameter cannot be casted to ParameterType</param>
-        public RelayCommand(Action<ParameterType> execute, Func<object, bool> canExecute = null, bool checkParamType = true)
-        {
-            _execute = execute;
-            _canExecute = canExecute;
-            _checkParamType = checkParamType;
-        }
+                if (o is ParameterType p)
+                    execute(p);
+                else 
+                    execute(default);
+            }, canExecute) { }
     }
 }
