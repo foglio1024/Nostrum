e9657472742443394271fd48bccd52b83fd80ee6
Foglio <foglio1024@gmail.com>
2021-05-12 11:16:50 +0200

    remove deprecated methods

--- Nostrum/Extensions/ByteExtensions.cs
+++ Nostrum/Extensions/ByteExtensions.cs
@@ -1,22 +1,14 @@
-﻿using System;
-
-namespace Nostrum.Extensions
+﻿namespace Nostrum.Extensions
 {
+    /// <summary>
+    /// Extension methods for the <see cref="byte"/> type.
+    /// </summary>
     public static class ByteExtensions
     {
-
         /// <summary>
         /// Converts the byte to its hex representation.
         /// </summary>
         public static string ToHexString(this byte b)
-        {
-#pragma warning disable 618
-            return ToStringEx(b);
-#pragma warning restore 618
-        }
-
-        [Obsolete("Use ToHexString() instead")]
-        public static string ToStringEx(this byte b)
         {
             return $"{b:x2}";
         }
