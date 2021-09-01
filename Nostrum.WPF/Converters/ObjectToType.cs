6c30a193bbc08497bd6aa6f761f9d8eff313f425
Foglio <foglio1024@gmail.com>
2021-09-01 08:57:07 +0200

    add ObjectToType converter

---
+++ Nostrum.WPF/Converters/ObjectToType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Nostrum.WPF.Converters
+{
+	public class ObjectToType : MarkupExtension, IValueConverter
+	{
+		private static ObjectToType? _instance;
+		public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return value?.GetType();
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override object ProvideValue(IServiceProvider serviceProvider)
+		{
+			if (_instance == null) _instance = new();
+			return _instance;
+		}
+	}
+}
