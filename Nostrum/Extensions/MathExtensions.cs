a8eece8fa92877deac2577498d915c0260ff248b
Foglio <foglio1024@gmail.com>
2022-01-31 09:51:24 +0100

    add extension methods for float and double operations

---
+++ Nostrum/Extensions/MathExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Nostrum.Extensions
+{
+    public static class MathExtensions
+    {
+        public static bool ToleranceEquals(this double val, double other, double tolerance)
+        {
+            return Math.Abs(val - other) < tolerance;
+        }
+
+		public static float ToDeg(this float rad)
+		{
+			return rad * 180f / (float)Math.PI;
+		}
+
+		public static double ToDeg(this double rad)
+		{
+			return rad * 180d / Math.PI;
+		}
+
+		public static float ToRad(this float deg)
+		{
+			return deg * (float)Math.PI / 180f;
+		}
+
+		public static double ToRad(this double deg)
+		{
+			return deg * Math.PI / 180d;
+		}
+	}
+}
