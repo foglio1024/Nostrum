acf4c385605df6d5796760ab53b7a45f3a6b0c23
Foglio <foglio1024@gmail.com>
2022-02-12 16:28:47 +0100

    add visual extensions

---
+++ Nostrum.WPF/Extensions/VisualExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace Nostrum.WPF.Extensions
+{
+    public static class VisualExtensions
+    {
+        public static DpiScale GetDpiScale(this Visual visual) //todo: docs
+        {
+            double dpiX = 1, dpiY = 1;
+
+            if (PresentationSource.FromVisual(visual) is PresentationSource source)
+            {
+                dpiX = source.CompositionTarget.TransformToDevice.M11;
+                dpiY = source.CompositionTarget.TransformToDevice.M22;
+            }
+
+            return new DpiScale(dpiX, dpiY);
+        }
+    }
+}
