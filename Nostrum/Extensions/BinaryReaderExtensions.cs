9f95b035f6d918e1c479252503b22b5ebd63071f
Foglio <foglio1024@gmail.com>
2022-06-07 11:27:39 +0200

    add BinaryWriter and BinaryReader extensions

---
+++ Nostrum/Extensions/BinaryReaderExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace Nostrum.Extensions;
+
+public static class BinaryReaderExtensions
+{
+	public static float ReadSingleBE(this BinaryReader br)
+	{
+		var data = br.ReadBytes(4);
+		Array.Reverse(data);
+		return BitConverter.ToSingle(data, 0);
+	}
+
+	public static ushort ReadUInt16BE(this BinaryReader br)
+	{
+		var data = br.ReadBytes(2);
+		Array.Reverse(data);
+		return BitConverter.ToUInt16(data, 0);
+	}
+
+	public static uint ReadUInt32BE(this BinaryReader br)
+	{
+		var data = br.ReadBytes(4);
+		Array.Reverse(data);
+		return BitConverter.ToUInt32(data, 0);
+	}
+	public static int ReadInt32BE(this BinaryReader br)
+	{
+		var data = br.ReadBytes(4);
+		Array.Reverse(data);
+		return BitConverter.ToInt32(data, 0);
+	}
+}
---
+++ Nostrum/Extensions/BinaryWriterExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace Nostrum.Extensions;
+
+public static class BinaryWriterExtensions
+{
+	public static void WriteBE(this BinaryWriter bw, ushort value)
+	{
+		var data = BitConverter.GetBytes(value);
+		Array.Reverse(data);
+		bw.Write(data);
+	}
+	public static void WriteBE(this BinaryWriter bw, uint value)
+	{
+		var data = BitConverter.GetBytes(value);
+		Array.Reverse(data);
+		bw.Write(data);
+	}
+	public static void WriteBE(this BinaryWriter bw, int value)
+	{
+		var data = BitConverter.GetBytes(value);
+		Array.Reverse(data);
+		bw.Write(data);
+	}
+}
